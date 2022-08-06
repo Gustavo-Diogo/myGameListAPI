dd880cd1b07db269c070973c5987827c5aeb2698
gustavo-diogo <gustavost1y@gmail.com>
2022-08-05 21:58:22 -0300

    feat: listing games.

--- src/controller/game/ListGamesUserController.ts
+++ src/controller/game/ListGamesUserController.ts
@@ -5,11 +5,26 @@ class ListGamesUserController {
 
     async execute(req: Request, res: Response) {
         try {
-            const games = await prisma.likedGame.findMany({
+            const likedGames = await prisma.likedGame.findMany({
                 where: {
                     userID: req.userId
                 }
             })
+
+            var games = []
+            const jogos = await prisma.game.findMany();
+
+            likedGames.forEach(e => {
+                jogos.find(j => {
+                    if (j.id == e.gameId) {
+                        games.push(j)
+                    }
+                })
+            })
+
+
+
+
             return res.status(200).json(games)
         } catch (err) {
             return res.status(400).json(err)
